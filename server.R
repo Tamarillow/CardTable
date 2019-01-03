5a10ae071cbc531d289501b9fd200a886ef67301
DavoSwain <d.swain@cqu.edu.au>
2019-01-03 09:57:48 +0100

    Using earlier version of a shiny app to provide the framework for the card table application

--- server.R
+++ server.R
@@ -1,33 +1,175 @@
+
+
+# Load libraries - need to make sure these packages are loaded via --------
+
 library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(geojsonio)
+library(mongolite)
+library(ggplot2)
+library(plotly)
+library(rgeos)
+library(DT)
+library(GISTools)
+library(dplyr)
+library(shinyWidgets)
+library(dygraphs)
+library(xts)
+library(shinyalert)
+library(sp)
+library(data.table)
 
 
-# Layout for table of cards
-shinyServer(function(input, output) {
-  
+PropPadds <<- geojsonio::geojson_read("~/Documents/RStudio/MarkMapping/paddocks/BelPadds.geojson", what = "sp")
 
-  output$cattledata <- DT::renderDT({
-    
-    # generate an rnorm distribution and plot it
-  inFile <- input$file1
+blip <- data.frame()
+
+catnums <- c(3442,
+             3399,
+             3845,
+             3784,
+             3344,
+             3859,
+             3348,
+             3067,
+             3667,
+             3224,
+             3179,
+             3473,
+             3139,
+             3495,
+             3588,
+             3152,
+             3444,
+             3781,
+             3024,
+             3502,
+             2943,
+             3243)
+
+
+
+#info = file.info(filenames)
+
+setwd("~/Desktop/TaggleLoc/")
+finf <- file.info(dir())
+finf <- finf[which(finf$size>50),]
+finf <- finf[which(difftime(Sys.time(), finf$mtime, units="days")<2),]
+chips <- rownames(finf)
+temp <- lapply(paste0("~/Desktop/TaggleLoc/",chips), fread, sep=",")
+MT <- rbindlist(temp)
+MT$V2 <- as.numeric(MT$V2)
+MT$V5 <- as.POSIXct(MT$V1,format="%d %B %Y %I:%M:%S")
+MT <- MT[which(MT$V2%in%catnums),]
+coords <- cbind(MT$V3,MT$V4)
+MTCattle <- SpatialPointsDataFrame(coords, MT)
+
+# ShinyServer -------------------------------------------------------------
+
+Shinyserver <- function(input, output, session) {
   
-  if (is.null(inFile))
-    return(NULL)
   
-  DT::datatable(read.csv(inFile$datapath, header = input$header), colnames=c("List"=2))
-    })
   
-  output$selectheader <- DT::renderDT({
-    inFile <- input$file1
+  
+  
+  
+  
+  #session$onSessionEnded(stopApp)
+  
+  
+  
+  
+  # Output sidebarpanel detail to UI  -------------------------------------------
+  
+  output$sidebarpanel <- renderUI({
     
-    if (is.null(inFile))
-      return(NULL)
-    jag <- read.csv(inFile$datapath, header = input$header)
-    jag <- jag[1,]
-    
-    DT::datatable(jag)
-    
+    div( width =400,
+         
+         fluidRow(box(width=12, solidHeader = TRUE, align = "center", height=80, tags$a(href='https://www.datamuster.net.au/',tags$img(src="DM1.png", height = 65, width = 200)))),
+         
+         fluidRow(box(width=12, height=110, status="success", (prettyRadioButtons("mapchoice", label="Map View Options:",
+                                                                                  choices = list("Base Map" = 1, "Satellite" = 2), selected = 1, inline=TRUE, outline= TRUE, animation = "pulse", status="danger")),
+                      prettyCheckbox(
+                        inputId = "displayboxes", label = "Display Cattle Info Box", shape = "round", status = "danger", outline = TRUE, value=FALSE, animation="pulse"
+                      ))),
+         #checkboxInput("displayboxes", "Display Map Info:", value=TRUE),
+         
+         fluidRow(box(width=12, height = 110,solidHeader = TRUE, prettyRadioButtons("zoomchoice", label = "Paddock Groups:",
+                                                                                    choices = list("Whole Property (no selection)" = 3, "Paddock" = 2, "Sentinel Cattle"=1),selected = 3, outline= TRUE, animation = "pulse", status="danger"))),
+         
+         fluidRow(box(width=12, height = 110, status="success", prettyRadioButtons("malefemale", label = "Males or Females:",
+                                                                                   choices = list("All Cattle (no selection)" = "all", "Males" = "male", "Females" = "female"),selected = "all", outline= TRUE, animation = "pulse", status="danger"))),
+         
+         fluidRow(box(width=12, height = 110, solidHeader=TRUE, prettyRadioButtons("breedersgrowers", label = "Breeders or Growers:",
+                                                                                   choices = list("All Cattle (no selection)"= "all", "Breeders" = "breeding", "Growers" = "growing"),selected = "all", outline= TRUE, animation = "pulse", status="danger"))),
+         fluidRow(box(width=12, height= 110, status="success", (prettyRadioButtons("radio", label = "Weight Range",
+                                                                                   choices = list("All Cattle (no selection)"= 1, "Weight Range" = 2), 
+                                                                                   selected = 1, outline= TRUE, animation = "pulse", status="danger")),
+                      tags$style(HTML(".js-irs-0 .irs-from, .irs-to {font-family: 'arial'; color:#414042; background:white; font-size:12px} .js-irs-0 .irs-min, .irs-max {font-family: 'arial'; color: #414042; background:white; font-size:12px} .js-irs-0 .irs-slider {width: 18px; height: 18px; top: 20px; background: #F15A29} .js-irs-0 .irs-bar {background: #F15A29}")), 
+                      sliderInput("rangewt", "ALMS live weight range (kg):",
+                                  min = 50, max = 1000, value = c(450,650))))
+    ) 
     
   })
   
-})
-
+  
+  # Output body detail to UI  -------------------------------------------    
+  output$body <- renderUI({
+    
+    
+    
+    div(width=12,
+        
+        # Info Box output --------------------------------------------------------------
+        
+        tags$head(
+          tags$style(HTML("hr {border-top: 2px solid #FCAF17;}"))
+        ),
+        fluidRow(
+          column(12,div(style = "height:25px;background-color: rgb(248,248,248);")
+          )),
+        fluidRow(width=12,
+                 
+                 box (title = h2("Belmont GPS Tracking Overview"), solidHeader = TRUE, status="warning", height = 850, width=12,
+                      
+                      leafletOutput("Map", height=750)
+                      
+                      
+                      
+                 ))
+        
+    )
+  })
+  
+  
+  
+  
+  
+  
+  
+  # Output Base Map Plus General Map Controls e.g. satelite and select paddocks ----------------------------------------     
+  
+  output$Map <- renderLeaflet({ leaflet(Map) %>%
+      fitBounds(PropPadds@bbox[1,1],PropPadds@bbox[2,1],PropPadds@bbox[1,2],PropPadds@bbox[2,2])%>%
+      clearMarkers()%>%
+      addPolygons(data=PropPadds, layerId=PropPadds@data$name,
+                  fillOpacity = 0.6, fillColor= ifelse(input$mapchoice==2,"white","white"), popup=paste0(PropPadds$name, " is ", round(PropPadds$hectares,digits=2), " Ha"), stroke=TRUE, color="#6D6E71",
+                  highlightOptions = highlightOptions(color = "#BE1E2D", weight = 3,
+                                                      bringToFront = FALSE))%>%
+      addMarkers(data=MTCattle, popup = paste0(MT$V1," ",MT$V2))
+    
+  })
+  
+  
+  observeEvent(input$mapchoice,{
+    satel <- input$mapchoice
+    leafletProxy("Map")%>%
+      #clearTiles()%>%
+      addProviderTiles(ifelse(satel==1,"OpenStreetMap.Mapnik","Esri.WorldImagery"))
+    
+  })
+  
+  
+  
+}
--- ui.R
+++ ui.R
@@ -1,30 +1,157 @@
 library(shiny)
-library(DT)
 library(shinydashboard)
+library(shiny)
+library(dashboardthemes)
 
-# Define UI for application that plots random distributions 
-shinyUI(fluidPage(
+
+### creating custom theme object
+theme_blue_gradient <- shinyDashboardThemeDIY(
   
-  # Application title
-  titlePanel("Tamallow Card Table"),
+  ### general
+  appFontFamily = "Helvetica"
+  ,appFontColor = "rgb(65,64,66)"
+  ,bodyBackColor = "rgb(248,248,248)"
   
-  # Sidebar with a slider input for number of observations
-  sidebarLayout(
-    sidebarPanel(
-    fileInput("file1", "Choose CSV File",
-              accept = c(
-                "text/csv",
-                "text/comma-separated-values,text/plain",
-                ".csv")
-    ),
-    tags$hr(),
-    checkboxInput("header", "Header", TRUE)
-  ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      fluidRow(DT::dataTableOutput("cattledata")),
-      fluidRow(DT::dataTableOutput("selectheader"))
-    )
+  ### header
+  ,logoBackColor = "rgb(241,90,41)"
+  
+  ,headerButtonBackColor = "rgb(241,90,41)"
+  ,headerButtonIconColor = "rgb(75,75,75)"
+  ,headerButtonBackColorHover = "rgb(210,210,210)"
+  ,headerButtonIconColorHover = "rgb(0,0,0)"
+  
+  ,headerBackColor = "rgb(241,90,41)"
+  ,headerBoxShadowColor = "#aaaaaa"
+  ,headerBoxShadowSize = "2px 2px 2px"
+  
+  ### sidebar
+  ,sidebarBackColor = cssGradientThreeColors(
+    direction = "down"
+    ,colorStart = "rgb(255,255,255)"
+    ,colorMiddle = "rgb(255,255,255)"
+    ,colorEnd = "rgb(255,255,255)"
+    ,colorStartPos = 0
+    ,colorMiddlePos = 50
+    ,colorEndPos = 100
   )
-))
+  ,sidebarPadding = 0
+  
+  ,sidebarMenuBackColor = "rgb(190,30,45)"
+  ,sidebarMenuPadding = 0
+  ,sidebarMenuBorderRadius = 10
+  
+  ,sidebarShadowRadius = "3px 5px 5px"
+  ,sidebarShadowColor = "#aaaaaa"
+  
+  ,sidebarUserTextColor = "rgb(253,214,138)"
+  
+  ,sidebarSearchBackColor = "rgb(55,72,80)"
+  ,sidebarSearchIconColor = "rgb(153,153,153)"
+  ,sidebarSearchBorderColor = "rgb(55,72,80)"
+  
+  ,sidebarTabTextColor = "rgb(253,214,138)"
+  ,sidebarTabTextSize = 13
+  ,sidebarTabBorderStyle = "none none solid none"
+  ,sidebarTabBorderColor = "rgb(35,106,135)"
+  ,sidebarTabBorderWidth = 1
+  
+  ,sidebarTabBackColorSelected = cssGradientThreeColors(
+    direction = "right"
+    ,colorStart = "rgba(44,222,235,1)"
+    ,colorMiddle = "rgba(44,222,235,1)"
+    ,colorEnd = "rgba(0,255,213,1)"
+    ,colorStartPos = 0
+    ,colorMiddlePos = 30
+    ,colorEndPos = 100
+  )
+  ,sidebarTabTextColorSelected = "rgb(253,214,138)"
+  ,sidebarTabRadiusSelected = "0px 20px 20px 0px"
+  
+  ,sidebarTabBackColorHover = cssGradientThreeColors(
+    direction = "right"
+    ,colorStart = "rgba(44,222,235,1)"
+    ,colorMiddle = "rgba(44,222,235,1)"
+    ,colorEnd = "rgba(0,255,213,1)"
+    ,colorStartPos = 0
+    ,colorMiddlePos = 30
+    ,colorEndPos = 100
+  )
+  ,sidebarTabTextColorHover = "rgb(50,50,50)"
+  ,sidebarTabBorderStyleHover = "none none solid none"
+  ,sidebarTabBorderColorHover = "rgb(75,126,151)"
+  ,sidebarTabBorderWidthHover = 1
+  ,sidebarTabRadiusHover = "0px 20px 20px 0px"
+  
+  ### boxes
+  ,boxBackColor = "rgb(255,255,255)"
+  ,boxBorderRadius = 0
+  ,boxShadowSize = "0px 0px 0px"
+  ,boxShadowColor = "rgba(0,0,0,.1)"
+  ,boxTitleSize = 16
+  ,boxDefaultColor = "rgb(253,214,138)"
+  ,boxPrimaryColor = "rgb(190,30,45)"
+  ,boxSuccessColor = "rgb(252,175,23)"
+  ,boxWarningColor = "rgb(252,175,23)"
+  ,boxDangerColor = "rgb(255,88,55)"
+  
+  ,tabBoxTabColor = "rgb(252,175,23)"
+  ,tabBoxTabTextSize = 14
+  ,tabBoxTabTextColor = "rgb(0,0,0)"
+  ,tabBoxTabTextColorSelected = "rgb(65,64,66)"
+  ,tabBoxBackColor = "rgb(255,255,255)"
+  ,tabBoxHighlightColor = "rgb(252,175,23)"
+  ,tabBoxBorderRadius = 5
+  
+  ### inputs
+  ,buttonBackColor = "rgb(245,245,245)"
+  ,buttonTextColor = "rgb(0,0,0)"
+  ,buttonBorderColor = "rgb(200,200,200)"
+  ,buttonBorderRadius = 5
+  
+  ,buttonBackColorHover = "rgb(235,235,235)"
+  ,buttonTextColorHover = "rgb(100,100,100)"
+  ,buttonBorderColorHover = "rgb(200,200,200)"
+  
+  ,textboxBackColor = "rgb(255,255,255)"
+  ,textboxBorderColor = "rgb(200,200,200)"
+  ,textboxBorderRadius = 5
+  ,textboxBackColorSelect = "rgb(245,245,245)"
+  ,textboxBorderColorSelect = "rgb(200,200,200)"
+  
+  ### tables
+  ,tableBackColor = "rgb(255,255,255)"
+  ,tableBorderColor = "rgb(240,240,240)"
+  ,tableBorderTopSize = 1
+  ,tableBorderRowSize = 1
+  
+)
+
+header <- dashboardHeader(titleWidth = 0)
+sidebar <- dashboardSidebar(uiOutput("sidebarpanel"), width=240
+                            
+                            
+                            
+)
+
+body <- dashboardBody(uiOutput("body"),
+                      
+                      tags$head(
+                        tags$style(HTML('
+                                        # .skin-blue .main-header .logo {
+                                        #             background-color: white;
+                                        #             }
+                                        #             .skin-blue .main-header .logo:hover {
+                                        #             background-color: white;
+                                        #             }
+                                        .box.box-solid.box-primary>.box-header {
+                                        color:#fff;
+                                        background:#666666
+                                        }
+                                        ')
+                        )
+                        ))
+
+ui <- dashboardPage(header, sidebar, body,   
+                    theme_blue_gradient
+                    
+)
